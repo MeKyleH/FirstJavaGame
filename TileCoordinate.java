18f24d06566ff27121de25f278df4fa82175e384
MeKyleH <kyle.a.holt@gmail.com>
2015-01-08 16:19:39 -0700

    Create TileCoordinate.java
    Used for placing tiles correctly.

---
+++ TileCoordinate.java
@@ -0,0 +1,30 @@
+package com.whoiskyleholt.level;
+
+public class TileCoordinate {
+
+	private int x, y;
+	private final int TILE_SIZE = 16;
+
+	public TileCoordinate(int x, int y) {
+		this.x = x * TILE_SIZE;
+		this.y = y * TILE_SIZE;
+	}
+
+	// returns x-coordinate
+	public int x() {
+		return x;
+	}
+
+	// returns y-coordinate
+	public int y() {
+		return y;
+	}
+
+	// returns x and y coordinates
+	public int[] xy() {
+		int[] r = new int[2];
+		r[0] = x;
+		r[1] = y;
+		return r;
+	}
+}
