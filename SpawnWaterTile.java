bfcf8921d08c357b2b2b5cd0f3e842cc3ccb176a
MeKyleH <kyle.a.holt@gmail.com>
2015-01-08 16:14:29 -0700

    Create SpawnWaterTile.java
    Used for displaying water.

---
+++ SpawnWaterTile.java
@@ -0,0 +1,17 @@
+package com.whoiskyleholt.level.tile.spawn_level;
+
+import com.whoiskyleholt.graphics.Screen;
+import com.whoiskyleholt.graphics.Sprite;
+import com.whoiskyleholt.level.tile.Tile;
+
+public class SpawnWaterTile extends Tile {
+
+	public SpawnWaterTile(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this); // starts at x, y, and renders the GrassTile (<<4 is the same as *16 for pixel conversion
+	}
+
+}
